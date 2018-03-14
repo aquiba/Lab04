47ff13ed00979245552f95620dd661a28f80fe18
aquiba <37160213+aquiba@users.noreply.github.com>
2018-03-14 12:07:54 -0700

    Update return.cpp

--- return.cpp
+++ return.cpp
@@ -63,6 +63,6 @@ void Return::display() const {
 // ---------- create() ----------
 // This is the create() function, and it creates a new
 // Return object for the Factory class to use.
-Transaction* Transaction::create() {
+Transaction* Return::create() {
         return new Return();
 } // end function create()
